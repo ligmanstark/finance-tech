8829056686b498dad40a5b8720e5d1d7a17d8bea
koziavin <koziavin@elma-bpm.com>
2025-03-30 16:54:00 +0400

    fix

--- frontend/src/pages/daily/index.vue
+++ frontend/src/pages/daily/index.vue
@@ -8,17 +8,48 @@
         Актуальный курс валют на текущую дату - {{ currentDate }}
       </UiParagraph>
     </template>
+    <UiSection>
+      <DataTable class="mt-5" :value="dailyData" tableStyle="min-width: 50rem">
+        <Column header="Валюта">
+          <template #body="slotProps">
+            <img
+              :src="getFlagUrl(slotProps.data.CharCode)"
+              :alt="slotProps.data.CharCode"
+              class="w-6 h-4 inline-block mr-2"
+            />
+            {{ slotProps.data.Name }}
+          </template>
+        </Column>
+        <Column header="Курс">
+          <template #body="slotProps">
+            <span> {{ slotProps.data.Value.toFixed(1) }}&nbsp;₽ </span>
+          </template>
+        </Column>
+        <Column class="text-center" header="Изменение курса">
+          <template #body="slotProps">
+            <span :class="delta(slotProps.data.Previous, slotProps.data.Value)">
+              {{ (slotProps.data.Value - slotProps.data.Previous).toFixed(3) }}&nbsp;₽
+            </span>
+          </template>
+        </Column>
+      </DataTable>
+    </UiSection>
   </UiSection>
 </template>
+
 <script>
 import UiSection from '@/shared/components/semantic/UiSection.vue'
 import UiHeading from '@/shared/components/typografi/UiHeading.vue'
 import UiParagraph from '@/shared/components/typografi/UiParagraph.vue'
+import { mapActions, mapState } from 'pinia'
+import { useDaily } from '@/entities/daily.store.js'
 
 export default {
-  name: 'dailyApp',
+  name: 'DailyApp',
   components: { UiParagraph, UiHeading, UiSection },
   computed: {
+    ...mapState(useDaily, ['dailyData']),
+
     currentDate() {
       return new Intl.DateTimeFormat('ru-RU', {
         day: '2-digit',
@@ -26,6 +57,46 @@ export default {
         year: 'numeric',
       }).format(new Date())
     },
+  },
+  methods: {
+    ...mapActions(useDaily, ['getDaily']),
+    async fetchData() {
+      try {
+        await this.getDaily()
+      } catch (error) {
+        console.error('Ошибка при загрузке данных:', error)
+      }
+    },
+    delta(prev, current) {
+      if (current - prev > 0) {
+        return 'text-green-600'
+      } else {
+        return 'text-red-600'
+      }
+    },
+    getCountryCode(currencyCode) {
+      const map = {
+        USD: 'us',
+        EUR: 'eu',
+        RUB: 'ru',
+        GBP: 'gb',
+        CNY: 'cn',
+        JPY: 'jp',
+        CAD: 'ca',
+        AUD: 'au',
+        CHF: 'ch',
+        TRY: 'tr',
+        KZT: 'kz',
+        UAH: 'ua',
+      }
+      return map[currencyCode] || 'un'
+    },
+    getFlagUrl(currencyCode) {
+      return `https://flagcdn.com/w40/${this.getCountryCode(currencyCode)}.png`
+    },
+  },
+  mounted() {
+    this.fetchData()
   },
 }
 </script>
